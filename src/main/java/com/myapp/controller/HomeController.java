9849e7bf35b9444ebf2021c0f6644db6d7034585
Kittipong Preboonpoon <kittipong.p@g-able.com>
2022-02-17 21:06:53 +0700

    Update test password

--- src/main/java/com/myapp/controller/HomeController.java
+++ src/main/java/com/myapp/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.myapp.controller;
 
+import com.myapp.service.car.DefaultCarService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -8,9 +12,21 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 public class HomeController {
 
+    private static org.slf4j.Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
+    @Value("${test.password}")
+    private String testPwd;
+
     @RequestMapping("/")
     public String home() {
         return "redirect:swagger-ui.html";
+    }
+
+    @RequestMapping("/test")
+    public void test() {
+        LOG.info("TestPWD=>"+testPwd);
+
+//        return testPwd;
     }
 
 }
